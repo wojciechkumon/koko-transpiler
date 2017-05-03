ce6de038489e72ed62dec79c1cd7f44ae436aef2
Wojciech Kumoń <wojtas626@gmail.com>
2017-05-03 12:45:45 +0200

    koko

--- src/newkoko/Interpreter.java
+++ src/newkoko/Interpreter.java
@@ -1,6 +1,7 @@
 package newkoko;
 
 import org.newkoko.analysis.DepthFirstAdapter;
+import org.newkoko.node.AAssignStatement;
 import org.newkoko.node.ADivExp;
 import org.newkoko.node.AGrammar;
 import org.newkoko.node.AMinusExp;
@@ -8,15 +9,13 @@ import org.newkoko.node.AMulExp;
 import org.newkoko.node.ANumberExp;
 import org.newkoko.node.APlusExp;
 import org.newkoko.node.Node;
-import org.newkoko.node.PExp;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class Interpreter extends DepthFirstAdapter {
   private Map<Node, Integer> node2int = new HashMap<>();
-  private Map<Node, Integer> vars = new HashMap<>();
+  private Map<String, Integer> vars = new HashMap<>();
 
   private int getNodeInt(Node node) {
     return node2int.get(node);
@@ -26,32 +25,47 @@ public class Interpreter extends DepthFirstAdapter {
     node2int.put(node, val);
   }
 
+  @Override
   public void outAGrammar(AGrammar node) {
-    Iterator<PExp> it = node.getExp().iterator();
-    while (it.hasNext()) {
-      PExp exp = it.next();
-      System.out.print(getNodeInt(exp));
-      if (it.hasNext()) System.out.print("; ");
-    }
+//    Iterator<PStatement> it = node.getStatement().iterator();
+//    while (it.hasNext()) {
+//      PStatement statement = it.next();
+//      System.out.print(getNodeInt(statement));
+//      if (it.hasNext()) System.out.print("; ");
+//    }
+    vars.forEach((key, value) -> System.out.println(key + " = " + value));
     System.out.println();
   }
 
+  @Override
+  public void outAAssignStatement(AAssignStatement node) {
+    int value = getNodeInt(node.getExp());
+    String identifier = node.getIdentifier().getText();
+    vars.put(identifier, value);
+  }
+
+
+  @Override
   public void outANumberExp(ANumberExp node) {
     setNodeInt(node, Integer.parseInt(node.getNumber().getText()));
   }
 
+  @Override
   public void outAPlusExp(APlusExp node) {
     setNodeInt(node, getNodeInt(node.getL()) + getNodeInt(node.getR()));
   }
 
+  @Override
   public void outAMinusExp(AMinusExp node) {
     setNodeInt(node, getNodeInt(node.getL()) - getNodeInt(node.getR()));
   }
 
-  public void outAMultExp(AMulExp node) {
+  @Override
+  public void outAMulExp(AMulExp node) {
     setNodeInt(node, getNodeInt(node.getL()) * getNodeInt(node.getR()));
   }
 
+  @Override
   public void outADivExp(ADivExp node) {
     // maybe we should check here for division by zero? :)
     setNodeInt(node, getNodeInt(node.getL()) / getNodeInt(node.getR()));
