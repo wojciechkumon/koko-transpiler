7b1055932501aec24648da68aa3da081a583689d
Wojciech Kumoń <wojtas626@gmail.com>
2017-05-03 15:47:19 +0200

    int + double, input from file

--- src/newkoko/NewKokoMain.java
+++ src/newkoko/NewKokoMain.java
@@ -5,6 +5,7 @@ import org.newkoko.node.Start;
 import org.newkoko.parser.Parser;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PushbackReader;
 
@@ -12,7 +13,11 @@ public class NewKokoMain {
 
   public static void main(String[] argv) {
     try {
-      Lexer l = new Lexer(new PushbackReader(new BufferedReader(new InputStreamReader(System.in))));
+      InputStream inputStream = NewKokoMain.class.getClassLoader().getResourceAsStream("test.koko");
+//      InputStream inputStream = System.in;
+
+
+      Lexer l = new Lexer(new PushbackReader(new BufferedReader(new InputStreamReader(inputStream))));
       Parser p = new Parser(l);
       Start start = p.parse();
 //      System.out.println(start.toString());
