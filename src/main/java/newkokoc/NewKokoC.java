be416d5f7c0f083a8c162323f0b00ecc6e653a43
Wojciech Kumoń <wojtas626@gmail.com>
2017-05-23 21:09:17 +0200

    Compiling list of files from stdin, build configured

--- src/main/java/newkokoc/NewKokoC.java
+++ src/main/java/newkokoc/NewKokoC.java
@@ -1,5 +1,6 @@
 package newkokoc;
 
+import org.apache.commons.io.FilenameUtils;
 import org.newkoko.c.lexer.Lexer;
 import org.newkoko.c.node.Start;
 import org.newkoko.c.parser.Parser;
@@ -7,30 +8,76 @@ import org.newkoko.c.parser.Parser;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PushbackReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 import newkoko.NewKokoMain;
 
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.READ;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+
 public class NewKokoC {
 
-  public static void main(String[] argv) {
-    try {
-      InputStream inputStream = NewKokoMain.class.getClassLoader().getResourceAsStream("test-input/new_koko_c.koko");
-//      InputStream inputStream = System.in;
+  public static void main(String[] argv) throws Exception {
+    if (argv.length == 0) {
+      InputStream inputStream = NewKokoMain.class.getClassLoader()
+          .getResourceAsStream("test-input/new_koko_c.koko");
+      singleCompile(inputStream, System.out);
+      return;
+    }
 
+    Arrays.asList(argv)
+        .forEach(NewKokoC::handleFile);
+  }
 
-      Lexer l = new Lexer(new PushbackReader(new BufferedReader(new InputStreamReader(inputStream))));
-      Parser p = new Parser(l);
-      Start start = p.parse();
-      System.out.println(start.toString());
+  private static void handleFile(String file) {
+    Path inputPath = Paths.get(file).toAbsolutePath();
+    if (!Files.exists(inputPath)) {
+      System.err.println("File " + file + " not exists!");
+      return;
+    }
+    if (!Files.isReadable(inputPath)) {
+      System.err.println("File " + file + " read protected!");
+      return;
+    }
+    Path outputPath = getOutFilePath(inputPath);
+    if (Files.exists(outputPath) && !Files.isWritable(outputPath)) {
+      System.err.println("File " + outputPath + " is write protected!");
+      return;
+    }
 
-      try (KokoCodeGenerator codeGenerator = new KokoCodeGenerator(System.out)) {
-        start.apply(codeGenerator);
+    compileFile(inputPath, outputPath);
+  }
+
+  private static Path getOutFilePath(Path inputPath) {
+    String outputFileName = FilenameUtils.getBaseName(inputPath.toString()) + ".c";
+    return inputPath.getParent().resolve(outputFileName);
+  }
+
+  private static void compileFile(Path inputPath, Path outputPath) {
+    try (InputStream in = Files.newInputStream(inputPath, READ)) {
+      try (OutputStream out = Files.newOutputStream(outputPath, CREATE, TRUNCATE_EXISTING)) {
+        singleCompile(in, out);
       }
-//      start.apply(new AstDisplayer());
-
+      System.out.println(inputPath.getFileName() + " compiled to " + outputPath.getFileName());
     } catch (Exception e) {
       e.printStackTrace();
     }
+  }
+
+  private static void singleCompile(InputStream in, OutputStream out) throws Exception {
+    Lexer l = new Lexer(new PushbackReader(new BufferedReader(new InputStreamReader(in))));
+    Parser p = new Parser(l);
+    Start start = p.parse();
+
+    try (KokoCodeGenerator codeGenerator = new KokoCodeGenerator(out)) {
+      start.apply(codeGenerator);
+    }
+    //      start.apply(new AstDisplayer());
   }
 }
